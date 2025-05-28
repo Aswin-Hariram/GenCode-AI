9253610ae2d122f548e2288563105b0afc6c5ee6
Aswin-Hariram <aswincseskct@gmail.com>
2025-05-29 00:45:08 +0530

    docker backend added

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Optional: Add a trailing slash to all paths
+  trailingSlash: true,
+  // Optional: Change the output directory
+  distDir: 'out',
+  // Optional: Optimize images for static export
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
